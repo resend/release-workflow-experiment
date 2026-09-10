4974ee8e8495a70e57f4e707fc95b71b9b1fa64e
Gabriel Miranda <gabriel@resend.com>
2026-09-10 17:37:55 -0300

    The module no longer is the version string (#43)

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-module.exports = require("./package.json").version;
-module.exports.version = module.exports;
+module.exports = { version: require("./package.json").version };
 module.exports.greet = (name) => `Hello, ${name || "world"}!`;
 module.exports.shout = (name) => module.exports.greet(name).toUpperCase();
 module.exports.whisper = require("./whisper.js").whisper;
