d0e77ed3dee8e64b396a69a0d75a291c82026bc6
Gabriel Miranda <gabriel@resend.com>
2026-09-10 17:21:06 -0300

    Add shout() (#26)
    (cherry picked from commit c9c838a83a8128b510d4b7675fd0aa0dc947c720)

---
+++ .tegami/add-shout.md
@@ -0,0 +1,9 @@
+---
+packages:
+  npm:release-workflow-experiment: minor
+backport-to: [1]
+---
+
+## Add shout()
+
+Like greet(), louder.
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports = require("./package.json").version;
 module.exports.version = module.exports;
 module.exports.greet = (name) => `Hello, ${name || "world"}!`;
+module.exports.shout = (name) => module.exports.greet(name).toUpperCase();
